84e887a454f69f8660c0257ef64f76d655e33770
Suyash <singhsuyash012@gmail.com>
2026-05-27 11:23:19 +0530

    fix: page buffer length

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  onDemandEntries: {
+    maxInactiveAge: 60 * 1000,
+    pagesBufferLength: 5,
+  },
 }
 
 export default nextConfig
