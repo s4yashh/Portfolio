8677c8bda8477100363abe334209d95228c18e05
Suyash <singhsuyash012@gmail.com>
2026-07-28 00:11:13 +0530

    feat(scroll): tune Lenis duration to 1.4s, reduce wheel/touch multipliers for fluid feel

--- components/smooth-scroll.tsx
+++ components/smooth-scroll.tsx
@@ -8,13 +8,14 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.4,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: "vertical",
       gestureOrientation: "vertical",
       smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
+      wheelMultiplier: 0.8,
+      touchMultiplier: 1.5,
+      infinite: false,
     })
 
     function raf(time: number) {
